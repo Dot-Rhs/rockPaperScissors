dbe2118edb304f9896c10c616a29c2b760e5e4c6
Dot-rhs <rhysh1986@gmail.com>
2021-03-05 20:08:37 +0000

    First push

---
+++ index.js
@@ -0,0 +1,8 @@
+const title = "Rock, Paper, Scissors?";
+var playerScore = 0;
+var compScore = 0;
+
+plyScr = document.getElementById("playerScore");
+cmpScr = document.getElementById("computerScore");
+
+// Have fun!
